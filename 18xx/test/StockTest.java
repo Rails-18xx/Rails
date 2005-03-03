68213d082af09f5360c642b13993272ede7b946e
Brett Lentz <brett.lentz@gmail.com>
2005-03-03 02:39:32 +0000

    Added test for displaying test stockchart swing class

--- 18xx/test/StockTest.java
+++ 18xx/test/StockTest.java
@@ -3,42 +3,55 @@
  */
 package test;
 
+import ui.StockChart;
 import game.*;
 
 /**
  * @author Brett Lentz
- *
+ *  
  */
-public class StockTest {
+public class StockTest
+{
 
-	public static void StockChartTest()
-	{
-		int row, col, price;
-		StockChart chart = new StockChart("1830");
-		StockPrice square;
-		
-		System.out.print (" ");
-		for (col=0; col<chart.getNumCols(); col++) {
-			System.out.print("   " + Character.toString((char)('A'+col)));
-		}
-		System.out.println();
-		for (row=0; row<chart.getNumRows(); row++) {
-			System.out.print ((row<9 ? " " : "") + (row+1)); 
-			for (col=0; col<chart.getNumCols(); col++) {
-				square = chart.getStockPrice(row,col);
-				if (square != null) {
-					price = square.getPrice();
-					System.out.print (" " + (price<100 ? " " : "") + price);
-				} else {
-					System.out.print("    ");
-				}
-			}
-			System.out.println();
-		}		
-	}
-	
-	public static void main(String[] args) {
+   public static void StockChartTest()
+   {
+      int row, col, price;
+      game.StockChart chart = new game.StockChart("1830");
+      StockPrice square;
 
-		StockChartTest();
-	}
+      System.out.print(" ");
+      for (col = 0; col < chart.getNumCols(); col++)
+      {
+         System.out.print("   " + Character.toString((char) ('A' + col)));
+      }
+      System.out.println();
+      for (row = 0; row < chart.getNumRows(); row++)
+      {
+         System.out.print((row < 9 ? " " : "") + (row + 1));
+         for (col = 0; col < chart.getNumCols(); col++)
+         {
+            square = chart.getStockPrice(row, col);
+            if (square != null)
+            {
+               price = square.getPrice();
+               System.out.print(" " + (price < 100 ? " " : "") + price);
+            } else
+            {
+               System.out.print("    ");
+            }
+         }
+         System.out.println();
+      }
+   }
+
+   public static void StockUITest()
+   {
+      StockChart sc = new ui.StockChart();
+   }
+
+   public static void main(String[] args)
+   {
+
+      StockUITest();
+   }
 }
