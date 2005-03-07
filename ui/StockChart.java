a9c64b480b5d44a2e2d0126f62eb8ff6418dd835
Brett Lentz <brett.lentz@gmail.com>
2005-03-07 23:33:55 +0000

    More testing out Swing

--- ui/StockChart.java
+++ ui/StockChart.java
@@ -1,73 +1,69 @@
 /*
- * Created on Feb 28, 2005
- */
+Rails: an 18xx game system.
+Copyright (C) 2005 Brett Lentz
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
 package ui;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.*;
 
-/**
- * @author Brett Lentz
- */ 
 public class StockChart extends JFrame
 {
-
    private int numRows, numCols, 
    				hgap, vgap, 
    				bhoriz, bvert;
-   private JPanel jContentPane = null;
    
-   private JButton up;
-   private JButton down;
-   private JButton fwd;
-   private JButton back;
-
-   /*
-    * adds values to the stockchart
-    * defaults to right-to-left placement
-    * 
-    * should we add stock objects to the chart, 
-    * and set toString()in the stock object to 
-    * get its value?
-    */
-   public void addToStockChart(int value)
-   {
-      jContentPane.add(new JLabel(Integer.toString(value)));
-   }
+   private Border lineBorder;
+   private JTextField text;
+   private JPanel stockPanel;
+   private JPanel statusPanel;
+   private JPanel buttonPanel;   
+   private JButton buyButton;
+   private JButton sellButton;
    
-   public void addToStockChart(String string)
-   {
-      jContentPane.add(new JLabel(string));
-   }
+   private GridBagConstraints gc;
    
    private void initialize()
    {
-      this.setSize(300, 200);
-      this.setContentPane(getJContentPane());
+      this.setSize(640, 480);
       this.setTitle("Stock Chart");
       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       
-      up   = new JButton("Up");
-      down = new JButton("Down");
-      fwd  = new JButton("Forward");
-      back = new JButton("Back");
+      lineBorder = BorderFactory.createLineBorder(Color.black);
+      text = new JTextField("Foo");      
+      text.setBorder(lineBorder);
+      text.setEditable(false);
       
-      up.setSize(bhoriz, bvert);
-      down.setSize(bhoriz, bvert);
-      fwd.setSize(bhoriz, bvert);
-      back.setSize(bhoriz, bvert);
+      stockPanel = new JPanel();
+      statusPanel = new JPanel();
+      buttonPanel = new JPanel();
+      
+      stockPanel.setBackground(Color.WHITE);
+      statusPanel.setBackground(Color.RED);
+      buttonPanel.setBackground(Color.LIGHT_GRAY);
+      
+      buyButton = new JButton("buy");
+      sellButton = new JButton("sell");
+      
+      gc = new GridBagConstraints();
    }
-
-   private JPanel getJContentPane()
-   {
-      if (jContentPane == null)
-      {
-         jContentPane = new javax.swing.JPanel();
-         jContentPane.setLayout(new GridLayout(numRows, numCols, hgap, vgap));
-      }
-      return jContentPane;
-   }
-   
+ 
    public StockChart()
    {
       super();
@@ -87,20 +83,37 @@ public class StockChart extends JFrame
       
       initialize();
       
-      for (int x = 0; x < 10; x++)
+      this.getContentPane().setLayout(new GridBagLayout());
+      
+      gc.gridx = 0;
+      gc.gridy = 0;
+      gc.fill = GridBagConstraints.BOTH;
+      this.getContentPane().add(stockPanel, gc);
+      
+      gc.gridx = 1;
+      gc.gridy = 1;
+      gc.fill = 0;
+      this.getContentPane().add(statusPanel, gc);
+      
+      gc.gridx = 0;
+      gc.gridy = 2;
+      gc.gridwidth = 2;
+      gc.fill = GridBagConstraints.HORIZONTAL;
+      this.getContentPane().add(buttonPanel, gc);
+      
+      stockPanel.setLayout(new GridLayout(numRows, numCols));
+            
+      for(int x=0; x<20; x++)
       {
-         addToStockChart(x);
+         stockPanel.add(new JTextField(Integer.toString(x)));
       }
       
-      addToStockChart("");
-      addToStockChart("");
+      statusPanel.add(text);
       
-      jContentPane.add(up);
-      jContentPane.add(down);
-      jContentPane.add(fwd);
-      jContentPane.add(back);
-      
+      buttonPanel.add(buyButton);
+      buttonPanel.add(sellButton);
+
       this.pack();
-      this.setVisible(true);
+      this.setVisible(true);      
    }
 }
