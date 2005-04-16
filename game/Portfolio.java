40c6853e91e6da24827f0f477526d7540e48d031
Erik Vos <erik.vos@xs4all.nl>
2005-04-16 23:03:08 +0000

    Was hard to get in...

--- game/Portfolio.java
+++ game/Portfolio.java
@@ -1,7 +1,7 @@
-/* $Header: /Users/blentz/rails_rcs/cvs/18xx/game/Attic/Portfolio.java,v 1.1 2005/04/16 22:51:22 evos Exp $
- * 
+/* $Header: /Users/blentz/rails_rcs/cvs/18xx/game/Attic/Portfolio.java,v 1.2 2005/04/16 23:03:08 evos Exp $
+ *
  * Created on 09-Apr-2005 by Erik Vos
- * 
+ *
  * Change Log:
  */
 package game;
@@ -12,50 +12,50 @@ import java.util.*;
  * @author Erik
  */
 public class Portfolio {
-	
+
 	/** Owned private companies */
 	protected ArrayList privateCompanies = new ArrayList();
-	
+
 	/** Owned public company certificates */
 	protected ArrayList certificates = new ArrayList();
 	/** Owned public company certificates, organised in a HashMap per company */
 	protected HashMap certPerCompany = new HashMap();
-	
+
 	/** Who owns the portfolio */
 	protected CashHolder owner;
 	/** Who receives the dividends (may differ from owner if that is the Bank) */
 	protected boolean paysToCompany = false;
-	
+
 	/** Name of portfolio */
 	protected String name;
-	
+
 	public Portfolio (String name, CashHolder holder, boolean paysToCompany) {
 		this.name = name;
 		this.owner = holder;
 		this.paysToCompany = paysToCompany;
 	}
-	
+
 	public Portfolio (String name, CashHolder holder) {
 		this.name = name;
 		this.owner = holder;
 	}
-	
+
 	public void buyPrivate (PrivateCompanyI privateCompany, Portfolio from, int price) {
-		
+
 		Log.write(getName()+" buys "+privateCompany.getName()
 			+" from "+from.getName()+" for "+price);
-			
+
 		// Move the private certificate
 		from.removePrivate (privateCompany);
 		this.addPrivate (privateCompany);
 		privateCompany.setHolder (this);
-		
+
 		// Move the money
 		Bank.transferCash(owner, from.owner, price);
 	}
-	
+
 	public void buyCertificate (CertificateI certificate, Portfolio from, int price) {
-		
+
 		Log.write (getName()+" buys "+certificate.getShare()+"% of "
 			+certificate.getCompany().getName()+" from "+from.getName()
 			+ " for "+price);
@@ -64,27 +64,27 @@ public class Portfolio {
 		from.removeCertificate (certificate);
 		this.addCertificate (certificate);
 		certificate.setPortfolio (this);
-		
+
 		// Move the money
 		Bank.transferCash(owner, from.owner, price);
 	}
-	
-	
+
+
 	public void addPrivate (PrivateCompanyI privateCompany) {
 		privateCompanies.add(privateCompany);
 		privateCompany.setHolder(this);
 	}
-	
+
 	public void addCertificate (CertificateI certificate) {
 		certificates.add(certificate);
 		String companyName = certificate.getCompany().getName();
 		if (!certPerCompany.containsKey(companyName)) {
-			certPerCompany.put(companyName, new ArrayList()); 
+			certPerCompany.put(companyName, new ArrayList());
 		}
 		((ArrayList)certPerCompany.get(companyName)).add(certificate);
 		certificate.setPortfolio(this);
 	}
-	
+
 	public boolean removePrivate (PrivateCompanyI privateCompany) {
 		for (int i=0; i<privateCompanies.size(); i++) {
 			if (privateCompanies.get(i) == privateCompany) {
@@ -94,7 +94,7 @@ public class Portfolio {
 		}
 		return false;
 	}
-	
+
 	public void removeCertificate (CertificateI certificate) {
 		for (int i=0; i<certificates.size(); i++) {
 			if (certificates.get(i) == certificate) {
@@ -108,17 +108,17 @@ public class Portfolio {
 				certs.remove(i);
 			}
 		}
-		
+
 	}
-	
+
 	public List getPrivateCompanies() {
 		return privateCompanies;
 	}
-	
+
 	public List getCertificates() {
 		return certificates;
 	}
-	
+
 	public List getCertificatesPerCompany (String compName) {
 		if (certPerCompany.containsKey(compName)) {
 			return (List) certPerCompany.get(compName);
@@ -126,25 +126,22 @@ public class Portfolio {
 			return new ArrayList();
 		}
 	}
-	
-	/** Find any non-president certificate */
+
+	/** Find any certificate */
 	public CertificateI findCertificate (PublicCompanyI company, boolean president) {
 		String companyName = company.getName();
-//System.out.println ("Looking for certificates of "+companyName+" in "+name);
 		if (!certPerCompany.containsKey(companyName)) return null;
 		Iterator it = ((List)certPerCompany.get(companyName)).iterator();
 		CertificateI cert;
 		while (it.hasNext()) {
 			 cert = (CertificateI) it.next();
-//System.out.println(".. found "+cert.getCompany().getName()+" "+cert.getShare()+"%");
 			 if (cert.getCompany() == company && president == cert.isPresident()) {
-//System.out.println("This one!");
 			 	  return cert;
 			 }
 		}
 		return null;
 	}
-	
+
 
 	/**
 	 * @return
@@ -184,7 +181,7 @@ public class Portfolio {
 	public String getName() {
 		return name;
 	}
-	
+
 	public int countShares (PublicCompanyI company) {
 		int share = 0;
 		String name = company.getName();
