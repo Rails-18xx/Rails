c8baeba1331a714cdfa6a0e458022093408c7c22
Brett Lentz <brett.lentz@gmail.com>
2005-09-22 23:27:57 +0000

    setClip() to fix bounds box issues.

--- ui/hexmap/GUIEWHex.java
+++ ui/hexmap/GUIEWHex.java
@@ -11,7 +11,7 @@ import ui.*;
 
 /**
  * Class GUIBattleHex holds GUI info for one hex with E-W orientation.
- * @version $Id: GUIEWHex.java,v 1.16 2005/09/22 23:22:47 wakko666 Exp $
+ * @version $Id: GUIEWHex.java,v 1.17 2005/09/22 23:27:57 wakko666 Exp $
  * @author David Ripton
  * @author Romain Dolbeau
  */
@@ -88,7 +88,7 @@ public class GUIEWHex extends GUIHex
     	//All adjustments to AffineTransform must be done before being assigned to the ATOp here.
     	AffineTransformOp aop = new AffineTransformOp(af, AffineTransformOp.TYPE_BILINEAR);    	  
     	    	
-     	//g2.setClip(hexagon);
+     	g2.setClip(hexagon);
     	g2.drawImage(tileImage, aop, (center.x + x_adjust), (center.y + y_adjust));
     	
     	if(arr_index == 6)
