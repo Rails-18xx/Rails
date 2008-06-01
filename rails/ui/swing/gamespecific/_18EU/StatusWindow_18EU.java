d8ed20f05e3b520f4b6f65f5286629af59e51023
Erik Vos <erik.vos@xs4all.nl>
2008-06-01 13:33:47 +0000

    Trial reformats

--- rails/ui/swing/gamespecific/_18EU/StatusWindow_18EU.java
+++ rails/ui/swing/gamespecific/_18EU/StatusWindow_18EU.java
@@ -1,6 +1,5 @@
-/* $Header: /Users/blentz/rails_rcs/cvs/18xx/rails/ui/swing/gamespecific/_18EU/StatusWindow_18EU.java,v 1.1 2008/03/05 19:55:13 evos Exp $*/
+/* $Header: /Users/blentz/rails_rcs/cvs/18xx/rails/ui/swing/gamespecific/_18EU/StatusWindow_18EU.java,v 1.2 2008/06/01 13:33:47 evos Exp $*/
 package rails.ui.swing.gamespecific._18EU;
-
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,27 +19,27 @@ import rails.util.LocalText;
  */
 public class StatusWindow_18EU extends StatusWindow {
 
-	@Override
-    protected boolean updateGameSpecificSettings () {
+    @Override
+    protected boolean updateGameSpecificSettings() {
 
-	    if (possibleActions.contains(DiscardTrain.class)) {
-	        immediateAction = possibleActions.getType(DiscardTrain.class).get(0);
-	    }
+        if (possibleActions.contains(DiscardTrain.class)) {
+            immediateAction = possibleActions.getType(DiscardTrain.class)
+                    .get(0);
+        }
 
         if (currentRound instanceof FinalMinorExchangeRound) {
-            setTitle(
-                    LocalText.getText("FinalMinorExchangeRoundTitle"));
+            setTitle(LocalText.getText("FinalMinorExchangeRoundTitle"));
             gameStatus.initTurn(GameManager.getCurrentPlayerIndex());
             return true;
         }
-		return false;
-	}
+        return false;
+    }
 
-	@Override
-	// Copied from StartRoundWindow, might become a generic function
-    public boolean processImmediateAction () {
+    @Override
+    // Copied from StartRoundWindow, might become a generic function
+    public boolean processImmediateAction() {
 
-        log.debug ("ImmediateAction="+immediateAction);
+        log.debug("ImmediateAction=" + immediateAction);
         if (immediateAction != null) {
             // Make a local copy and discard the original,
             // so that it's not going to loop.
@@ -56,33 +55,30 @@ public class StatusWindow_18EU extends StatusWindow {
                 List<String> trainOptions = new ArrayList<String>(trains.size());
                 String[] options = new String[trains.size()];
 
-                for (int i=0; i<options.length; i++) {
-                    options[i] = LocalText.getText("N_Train", trains.get(i).getName());
+                for (int i = 0; i < options.length; i++) {
+                    options[i] = LocalText.getText("N_Train", trains.get(i)
+                            .getName());
                     trainOptions.add(options[i]);
                 }
-                String discardedTrainName = (String) JOptionPane.showInputDialog (
-                        this,
-                        LocalText.getText("HAS_TOO_MANY_TRAINS", new String[] {
-                                playerName,
-                                c.getName()
-                        }),
-                        LocalText.getText("WhichTrainToDiscard"),
-                        JOptionPane.QUESTION_MESSAGE,
-                        null,
-                        options,
-                        options[0]);
-                if (discardedTrainName != null)
-                {
-                    TrainI discardedTrain = trains.get(trainOptions.indexOf(discardedTrainName));
+                String discardedTrainName = (String) JOptionPane
+                        .showInputDialog(this, LocalText.getText(
+                                "HAS_TOO_MANY_TRAINS", new String[] {
+                                        playerName, c.getName() }), LocalText
+                                .getText("WhichTrainToDiscard"),
+                                JOptionPane.QUESTION_MESSAGE, null, options,
+                                options[0]);
+                if (discardedTrainName != null) {
+                    TrainI discardedTrain = trains.get(trainOptions
+                            .indexOf(discardedTrainName));
 
                     dt.setDiscardedTrain(discardedTrain);
 
-                    process (dt);
+                    process(dt);
                 }
             }
         }
         return true;
 
-	}
+    }
 
 }
