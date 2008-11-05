e9b76f07559d5357079f72b2eefda52c9b782d17
Erik Vos <erik.vos@xs4all.nl>
2008-11-05 22:44:27 +0000

    Fixed 1856 float percentage (was incorrectly linked to phase rather than next available train)

--- 18xx/rails/game/specific/_1856/StockRound_1856.java
+++ 18xx/rails/game/specific/_1856/StockRound_1856.java
@@ -2,6 +2,8 @@ package rails.game.specific._1856;
 
 import rails.game.PublicCompanyI;
 import rails.game.StockRound;
+import rails.game.TrainI;
+import rails.game.TrainManager;
 
 public class StockRound_1856 extends StockRound {
 
@@ -15,13 +17,17 @@ public class StockRound_1856 extends StockRound {
         if (!company.hasStarted() || company.hasFloated()) return;
 
         int unsoldPercentage = company.getUnsoldPercentage();
-        int floatPercentage = getCurrentPhase().getParameterAsInteger("floatPercentage");
         
-        if (floatPercentage == 0) {
-            log.error ("FloatPercentage is null!");
-        } else {
-            log.debug ("Floatpercentage is "+floatPercentage);
+        TrainI nextAvailableTrain = TrainManager.get().getAvailableNewTrains().get(0);
+        int trainNumber;
+        try { 
+            trainNumber = Integer.parseInt(nextAvailableTrain.getName());
+        } catch (NumberFormatException e) {
+            trainNumber = 6; // Diesel!
         }
+        int floatPercentage = 10 * trainNumber;
+        
+        log.debug ("Floatpercentage is "+floatPercentage);
         
         //int floatPercentage = Math.min(60, (Integer)getCurrentPhase().getParameter("floatPercentage"));
 
