35f6648521b929500de3f2e46151cc71177cab59
Erik Vos <erik.vos@xs4all.nl>
2009-05-24 18:44:20 +0000

    Removed outcommented code

--- rails/game/specific/_1856/GameUIManager_1856.java
+++ rails/game/specific/_1856/GameUIManager_1856.java
@@ -1,17 +1,8 @@
 package rails.game.specific._1856;
 
-import rails.game.action.PossibleActions;
-import rails.ui.swing.ActionPerformer;
 import rails.ui.swing.GameUIManager;
 
 public class GameUIManager_1856 extends GameUIManager {
 
-    /*
-    protected void updateStatus(ActionPerformer activeWindow) {
-        
-        PossibleActions possibleActions = PossibleActions.getInstance();
-        
-    }
-    */
 
 }
