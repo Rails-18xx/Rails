481abdf87e1ebe74f8385f8cd76dde0366e51c6b
Erik Vos <erik.vos@xs4all.nl>
2009-08-30 20:50:29 +0000

    Use Unicode \u00bd for ½ (0.5)

--- rails/game/model/CertCountModel.java
+++ rails/game/model/CertCountModel.java
@@ -1,4 +1,4 @@
-/* $Header: /Users/blentz/rails_rcs/cvs/18xx/rails/game/model/CertCountModel.java,v 1.6 2009/08/30 19:28:34 evos Exp $*/
+/* $Header: /Users/blentz/rails_rcs/cvs/18xx/rails/game/model/CertCountModel.java,v 1.7 2009/08/30 20:50:29 evos Exp $*/
 package rails.game.model;
 
 import rails.game.Player;
@@ -12,7 +12,7 @@ public class CertCountModel extends ModelObject {
     }
 
     public String getText() {
-        return ("" + owner.getPortfolio().getCertificateCount()).replaceFirst(".0", "").replaceFirst(".5", "½");
+        return ("" + owner.getPortfolio().getCertificateCount()).replaceFirst(".0", "").replaceFirst(".5", "\u00bd");
     }
 
 }
