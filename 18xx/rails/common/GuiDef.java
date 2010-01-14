b2edc1d609f08bf08c0b1cd55dce3b7530be38a9
Erik Vos <erik.vos@xs4all.nl>
2010-01-14 20:40:26 +0000

    Renamed

--- 18xx/rails/common/GuiDef.java
+++ 18xx/rails/common/GuiDef.java
@@ -1,22 +1,22 @@
 package rails.common;
 
-public class Defs {
+public class GuiDef {
 
     /** Identifiers and default names for configurable UI classes */
     public enum ClassName {
-        
+
         GAME_UI_MANAGER ("rails.ui.swing.GameUIManager"),
         OR_UI_MANAGER ("rails.ui.swing.ORUIManager"),
         STATUS_WINDOW ("rails.ui.swing.StatusWindow"),
         GAME_STATUS ("rails.ui.swing.GameStatus"),
         OR_WINDOW ("rails.ui.swing.ORWindow");
-        
+
         private String defaultClassName;
-        
+
         ClassName (String defaultClassName) {
             this.defaultClassName = defaultClassName;
         }
-        
+
         public String getDefaultClassName () {
             return defaultClassName;
         }
@@ -37,5 +37,17 @@ public class Defs {
         CAN_ANY_COMPANY_BUY_PRIVATES,
         DO_BONUS_TOKENS_EXIST,
         HAS_ANY_COMPANY_LOANS
+    }
+
+    /**
+     * Definitions for UI window types, used by the server
+     * to pass visibility hints to the UI.
+     */
+    public enum Panel {
+
+    	START_ROUND,
+    	STATUS,
+    	MAP,
+    	STOCK_MARKET
     }
 }
