885db40483ce9940b3998999595dc34cba4fd5b5
Erik Vos <erik.vos@xs4all.nl>
2010-01-14 21:03:19 +0000

    New class, for game-engine parameter definitions

---
+++ 18xx/rails/game/GameDef.java
@@ -0,0 +1,26 @@
+package rails.game;
+
+/**
+ * This class will hold parameter identifiers for use in the game engine only.
+ * See rails.common.Defs for parameters used in the GUI/engine communication.
+ * @author VosE
+ *
+ */
+public class GameDef {
+
+	public enum Parm {
+
+		NO_SALE_IN_FIRST_SR (false),
+		NO_SALE_IF_NOT_OPERATED (false),
+		STOCK_ROUND_SEQUENCE(StockRound.SELL_BUY_SELL);
+
+		private Object defaultValue;
+
+		Parm (boolean defaultValue) { this.defaultValue = defaultValue; }
+		Parm (int defaultValue) { this.defaultValue = defaultValue; }
+
+		public boolean defaultValueAsBoolean() { return (Boolean) defaultValue; }
+		public int defaultValueAsInt() { return (Integer) defaultValue; }
+	}
+
+}
