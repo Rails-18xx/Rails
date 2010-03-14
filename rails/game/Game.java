a569b3418b8f9258693472bdfd9d0c5f6143b728
Brett Lentz <brett.lentz@gmail.com>
2010-03-14 19:29:07 +0000

    Update files with version number 1.2

--- rails/game/Game.java
+++ rails/game/Game.java
@@ -1,4 +1,4 @@
-/* $Header: /Users/blentz/rails_rcs/cvs/18xx/rails/game/Game.java,v 1.49 2010/02/11 21:55:30 evos Exp $ */
+/* $Header: /Users/blentz/rails_rcs/cvs/18xx/rails/game/Game.java,v 1.50 2010/03/14 19:29:07 wakko666 Exp $ */
 package rails.game;
 
 import java.io.*;
@@ -11,7 +11,7 @@ import rails.util.LocalText;
 import rails.util.Tag;
 
 public class Game {
-    public static final String version = "1.1.3+";
+    public static final String version = "1.2";
 
     /** The component Manager */
     protected ComponentManager componentManager;
