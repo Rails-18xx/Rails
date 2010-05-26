e3dbfb1540a7f93a86eafae410846d36835ee2f1
Stefan Frey <stefan.frey@web.de>
2010-05-26 19:48:57 +0000

    Added optimizing option for named trains in 18AL

--- rails/game/specific/_18AL/NamedTrainRevenueModifier.java
+++ rails/game/specific/_18AL/NamedTrainRevenueModifier.java
@@ -1,15 +1,39 @@
 package rails.game.specific._18AL;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import rails.algorithms.NetworkTrain;
 import rails.algorithms.NetworkVertex;
 import rails.algorithms.RevenueAdapter;
 import rails.algorithms.RevenueBonus;
+import rails.algorithms.RevenueDynamicModifier;
 import rails.algorithms.RevenueStaticModifier;
+import rails.algorithms.RevenueTrainRun;
+import rails.game.ConfigurableComponentI;
+import rails.game.ConfigurationException;
+import rails.game.GameManagerI;
 import rails.game.TrainI;
+import rails.util.Tag;
 
-public class NamedTrainRevenueModifier implements RevenueStaticModifier {
+public class NamedTrainRevenueModifier implements RevenueStaticModifier, RevenueDynamicModifier, ConfigurableComponentI {
 
+    private boolean dynamic;
+    private List<RevenueBonus> bonuses;
+    private int bonusMaximum;
+    
+    public void configureFromXML(Tag tag) throws ConfigurationException {
+        // do nothing
+    }
+
+    public void finishConfiguration(GameManagerI parent)
+            throws ConfigurationException {
+        dynamic = parent.getGameOption("18ALOptimizeNamedTrains").equalsIgnoreCase("yes");
+    }
+    
     public void modifyCalculator(RevenueAdapter revenueAdapter) {
+        // static modifier
+        if (dynamic) return;
         
         // 1. check all Trains for name Tokens
         for (NetworkTrain networkTrain:revenueAdapter.getTrains()) {
@@ -27,5 +51,64 @@ public class NamedTrainRevenueModifier implements RevenueStaticModifier {
             revenueAdapter.addRevenueBonus(bonus);
         }
     }
+
+    public boolean prepareModifier(RevenueAdapter revenueAdapter) {
+        // dynamic modifier
+        if (!dynamic) return false;
+        
+        // 1. check if name trains special properties is available
+        List<NameTrains> sp = revenueAdapter.getCompany().getPortfolio().getSpecialProperties(NameTrains.class, false);
+        if (sp.isEmpty()) return false;
+        
+        // 2. prepare by defining the vertices
+        bonuses = new ArrayList<RevenueBonus>();
+        bonusMaximum = 0;
+        // 3. there is only one special property in 18AL, thus get tokens from it
+        for (NamedTrainToken token:sp.get(0).getTokens()) {
+            RevenueBonus bonus = new RevenueBonus(token.getValue(), token.getLongName());
+            // 4. define vertices
+            for (NetworkVertex vertex:NetworkVertex.getVerticesByHexes(revenueAdapter.getVertices(), token.getHexesToPass())) {
+                if (!vertex.isStation()) continue;
+                bonus.addVertex(vertex);
+            }
+            bonuses.add(bonus);
+            bonusMaximum += token.getValue();
+        }
+        return true;
+    }
+
+    public int predictionValue(RevenueAdapter revenueAdapter) {
+        return bonusMaximum;
+    }
+
+    public int evaluationValue(RevenueAdapter revenueAdapter) {
+        List<RevenueTrainRun> runs = revenueAdapter.getCurrentRun();
+        int bonusValue = 0;
+        // due to the geography each train can only score one bonus
+        for (RevenueBonus bonus:bonuses) {
+            for (RevenueTrainRun run:runs) {
+                if (run.getUniqueVertices().containsAll(bonus.getVertices())) {
+                    bonusValue += bonus.getValue();
+                    continue; // each bonus can only be scored once
+                }
+            }
+        }
+        return bonusValue;
+    }
+
+    public String prettyPrint(RevenueAdapter revenueAdapter) {
+        List<RevenueTrainRun> runs = revenueAdapter.getOptimalRun();
+        StringBuffer prettyPrint = new StringBuffer();
+        for (RevenueBonus bonus:bonuses) {
+            for (RevenueTrainRun run:runs) {
+                if (run.getUniqueVertices().containsAll(bonus.getVertices())) {
+                    prettyPrint.append(bonus.getName() + ": " + bonus.getValue() + "\n");
+                    continue; // each bonus can only be scored once
+                }
+            }
+        }
+        return prettyPrint.toString();
+    }
+
 
 }
