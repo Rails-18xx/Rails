d064d858ecf8f5e44ba48bd215dfde99a24cb274
Stefan Frey <stefan.frey@web.de>
2010-07-17 20:27:23 +0000

    Update configuration management

--- rails/ui/swing/ConfigWindow.java
+++ rails/ui/swing/ConfigWindow.java
@@ -2,29 +2,49 @@ package rails.ui.swing;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.EventQueue;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.io.File;
 import java.util.Map;
 import java.util.List;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JColorChooser;
+import javax.swing.JComboBox;
 import javax.swing.JComponent;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
 import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSpinner;
 import javax.swing.JTabbedPane;
+import javax.swing.SpinnerListModel;
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
+import rails.game.ConfigurationException;
 import rails.util.Config;
 import rails.util.ConfigItem;
 import rails.util.LocalText;
+import rails.util.Util;
 
 class ConfigWindow extends JFrame {
     private static final long serialVersionUID = 1L;
     
-    private JTabbedPane pane;
+    private JPanel profilePanel;
+    private JTabbedPane configPane;
+    private JPanel buttonPanel;
     
     ConfigWindow() {
        // JFrame properties
@@ -32,54 +52,65 @@ class ConfigWindow extends JFrame {
 //        setSize(400,300);
         
         // add profile panel
-        add(setupProfilePanel(), "North");
+        profilePanel = new JPanel();
+        add(profilePanel, "North");
         
         // configSetup pane
-        setupConfigPane();
-        add(pane, "Center");
+        configPane = new JTabbedPane();
+        add(configPane, "Center");
  
         // buttons
-        JPanel buttonPanel = new JPanel();
-        
-        JButton saveButton = new JButton(LocalText.getText("Save"));
-        saveButton.addActionListener(
-                new ActionListener() {
-                    public void actionPerformed(ActionEvent arg0) {
-                        ConfigWindow.this.saveConfig();
-                    }
-                }
-        );
-        buttonPanel.add(saveButton);
-
-        JButton cancelButton = new JButton(LocalText.getText("Cancel"));
-        cancelButton.addActionListener(
-                new ActionListener() {
-                    public void actionPerformed(ActionEvent arg0) {
-                        ConfigWindow.this.cancelConfig();
-                    }
-                }
-        );
-        buttonPanel.add(cancelButton);
-
+        buttonPanel = new JPanel();
         add(buttonPanel, "South");
-        
+
+        init();
+    }
+    
+    private void init() {
+        setupProfilePanel();
+        setupConfigPane();
+        setupButtonPanel();
         this.pack();
     }
 
-    
-    private JComponent setupProfilePanel() {
-        JComponent panel = new JPanel();
-        panel.setLayout(new GridLayout(0,4));
+    private void setupProfilePanel() {
+        profilePanel.removeAll();
         
-        // default profile
+        profilePanel.setLayout(new GridLayout(0,4));
         
+        String activeProfile = Config.getActiveProfileName();
+        String defaultProfile = Config.getDefaultProfileName();
+        Border etched = BorderFactory.createEtchedBorder();
+        Border titled = BorderFactory.createTitledBorder(etched, LocalText.getText("CONFIG_CURRENT_PROFILE", activeProfile, defaultProfile));
+        profilePanel.setBorder(titled);
         
-        return panel;
+        JLabel userLabel = new JLabel(LocalText.getText("CONFIG_SELECT_USER"));
+        profilePanel.add(userLabel);
+        final JComboBox comboBoxUser = new JComboBox(Config.getUserProfiles().toArray());
+        comboBoxUser.setSelectedItem(Config.getActiveProfileName());
+        comboBoxUser.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent arg0) {
+                Config.changeActiveProfile((String)comboBoxUser.getSelectedItem());
+                EventQueue.invokeLater(new Runnable() {
+                    public void run() {
+                        init();
+                        ConfigWindow.this.repaint();
+                    }
+                }
+                );
+            }
+        }
+        );
+        profilePanel.add(comboBoxUser);
+
     }
     
     private void setupConfigPane() {
-        // create pane
-        pane = new JTabbedPane();
+        configPane.removeAll();
+        
+        Border etched = BorderFactory.createEtchedBorder();
+        Border titled = BorderFactory.createTitledBorder(etched, LocalText.getText("CONFIG_SETTINGS"));
+        configPane.setBorder(titled);
         
         Map<String, List<ConfigItem>> configPanels = Config.getConfigPanels();
         
@@ -89,52 +120,151 @@ class ConfigWindow extends JFrame {
             for (ConfigItem item:configPanels.get(panelName)) {
                 defineElement(newPanel, item);
             }
-            pane.addTab(panelName, newPanel);
+            configPane.addTab(panelName, newPanel);
         }
     }
     
-    private void defineElement(JPanel panel, ConfigItem item) {
-        
+    private void defineElement(JPanel panel, final ConfigItem item) {
+        // item label
         panel.add(new JLabel(LocalText.getText("Config." + item.name)));
         
-        final String configValue = Config.get(item.name);
+        // standard components
+        final String configValue = item.getCurrentValue();
+
+        
         switch (item.type) {
-        case COLOR: {
+        case COLOR:
             final JLabel label = new JLabel(configValue);
             Color selectedColor;
             try {
-                selectedColor = Color.decode(configValue);
-            } catch (NumberFormatException e) {
+                selectedColor = Util.parseColour(configValue);
+            } catch (ConfigurationException e) {
                selectedColor = Color.WHITE;
             }
             label.setOpaque(true);
+            label.setHorizontalAlignment(SwingConstants.CENTER);
             label.setBackground(selectedColor);
+            if (Util.isDark(selectedColor)) {
+                label.setForeground(Color.WHITE);
+            } else {
+                label.setForeground(Color.BLACK);
+            }
             panel.add(label);
-            JButton button = new JButton("Color");
+            JButton button = new JButton("Choose...");
             final Color oldColor = selectedColor; 
             button.addActionListener(
                     new ActionListener() {
                         public void actionPerformed(ActionEvent e) {
                             Color selectedColor=JColorChooser.showDialog(ConfigWindow.this, "", oldColor);
-                            label.setText(Integer.toHexString(selectedColor.getRGB()).substring(2));
+                            if (selectedColor == null) return;
+                            String newValue = Integer.toHexString(selectedColor.getRGB()).substring(3); 
+                            label.setText(newValue);
+                            item.setNewValue(newValue);
                             label.setBackground(selectedColor);
+                            if (Util.isDark(selectedColor)) {
+                                label.setForeground(Color.WHITE);
+                            } else {
+                                label.setForeground(Color.BLACK);
+                            }
                         }
                     }
             );
             panel.add(button);
             break;
-        }
+        case LIST:
+            final JComboBox comboBox = new JComboBox(item.allowedValues.toArray());
+            comboBox.setSelectedItem(configValue);
+            comboBox.addFocusListener(new FocusListener() {
+                public void focusGained(FocusEvent arg0) {
+                    // do nothing
+                }
+                public void focusLost(FocusEvent arg0) {
+                  item.setNewValue((String)comboBox.getSelectedItem());
+                }
+            }
+            );
+            panel.add(comboBox);
+            break;
         case STRING:
-        default: {
-            JFormattedTextField textField = new JFormattedTextField();
+        default: // default like String
+            final JFormattedTextField textField = new JFormattedTextField();
             textField.setValue(configValue);
+            textField.addFocusListener(new FocusListener() {
+                public void focusGained(FocusEvent arg0) {
+                    // do nothing
+                }
+                public void focusLost(FocusEvent arg0) {
+                  item.setNewValue(textField.getText());
+                }
+            }
+            );
             panel.add(textField);
+            break;
         }
+    }
+
+    private void setupButtonPanel() {
+        buttonPanel.removeAll();
+        
+        // save button
+        if (Config.isFilePathDefined()) {
+            JButton saveButton = new JButton(LocalText.getText("SAVE"));
+            saveButton.addActionListener(
+                    new ActionListener() {
+                        public void actionPerformed(ActionEvent arg0) {
+                            ConfigWindow.this.saveConfig();
+                        }
+                    }
+            );
+            buttonPanel.add(saveButton);
         }
+
+        JButton saveAsButton = new JButton(LocalText.getText("SAVEAS"));
+        saveAsButton.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent arg0) {
+                        ConfigWindow.this.saveAsConfig();
+                    }
+                }
+        );
+        buttonPanel.add(saveAsButton);
+
+        JButton cancelButton = new JButton(LocalText.getText("CANCEL"));
+        cancelButton.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent arg0) {
+                        ConfigWindow.this.cancelConfig();
+                    }
+                }
+        );
+        buttonPanel.add(cancelButton);
+        
     }
     
     private void saveConfig() {
         this.dispose();
+    }
+
+    private void saveAsConfig() {
+        JFileChooser fc = new JFileChooser();
+        fc.setFileFilter(
+                new FileFilter() {
+                    public boolean accept( File f ){
+                        return f.isDirectory() ||
+                        f.getName().toLowerCase().endsWith( ".rails_config" );
+                    }
+                    public String getDescription() {
+                        return "Rails Config";
+                    }
+                }
+        );
+
+        int state = fc.showOpenDialog( null );
+        if ( state == JFileChooser.APPROVE_OPTION )
+        {
+            File file = fc.getSelectedFile();
+            Config.setActiveFilepath(file.getPath());
+        }
     }
 
     private void cancelConfig() {
--- rails/util/ConfigItem.java
+++ rails/util/ConfigItem.java
@@ -2,20 +2,37 @@ package rails.util;
 
 import java.util.Arrays;
 import java.util.List;
+
+import org.apache.log4j.Logger;
+
 import rails.game.ConfigurationException;
-import rails.util.Config.ConfigType;
 
 /**
  * Defines an item used for the configuration of rails
  * T represents the value type
  */
 
-public class ConfigItem {
+public final class ConfigItem {
+
+    protected static Logger log =
+        Logger.getLogger(ConfigItem.class.getPackage().getName());
+
+    /**
+     * Defines possible types (Java classes used as types in ConfigItem below
+     */
+    public static enum ConfigType {
+        INTEGER, FLOAT, STRING, BOOLEAN, LIST, DIRECTORY, COLOR;
+    }
+    
+    // static attributes
     public final String name;
     public final ConfigType type;
     public final List<String> allowedValues;
     public final String formatMask;
     public final String helpText;
+    
+    // dynamic attributes
+    private String newValue;
     
     ConfigItem(Tag tag) throws ConfigurationException {
         // check name and type (required)
@@ -25,29 +42,68 @@ public class ConfigItem {
         } else {
             throw new ConfigurationException("Missing name for configuration item");
         }
-        String type = tag.getAttributeAsString("type");
-        if (Util.hasValue(type)) {
-            this.type = ConfigType.valueOf(type);
-        } else {
-            throw new ConfigurationException("Missing or invalid type for configuration item");
-        }
         // optional: list of allowed values
         String valueString = tag.getAttributeAsString("values");
         if (Util.hasValue(valueString)) {
             allowedValues = Arrays.asList(valueString.split(","));
+            this.type = ConfigType.LIST;
         } else {
             allowedValues = null;
+            String type = tag.getAttributeAsString("type");
+            if (Util.hasValue(type)) {
+                try {
+                    this.type = ConfigType.valueOf(type.toUpperCase());
+                } catch (Exception e) {
+                    throw new ConfigurationException("Missing or invalid type for configuration item");
+                }
+            } else {
+                throw new ConfigurationException("Missing or invalid type for configuration item");
+            }
+            if (this.type == ConfigType.LIST) {
+                throw new ConfigurationException("No values defined for LIST config item");
+            }
         }
+        
         // optional: formatMask
         formatMask = tag.getAttributeAsString("formatMask");
 
         // optional: helpText
         helpText = tag.getAttributeAsString("formatMask");
+        
+        newValue = null;
+    }
+    
+    public boolean hasNewValue() {
+        return (newValue != null);
+    }
+    
+    public String getNewValue() {
+        return newValue;
+    }
+    
+    public void setNewValue(String newValue) {
+        if (newValue.equals(getConfigValue())) {
+            this.newValue = null;
+        } else {
+            this.newValue = newValue;
+        }
+        log.debug("ConfigItem " + name + " set to new value " + this.newValue);
+    }
+    
+    public String getCurrentValue() {
+        if (newValue != null) return newValue;
+        return getConfigValue();
+    }
+    
+    public String getConfigValue() {
+        return Config.get(this.name);
     }
     
     public String toString() {
         StringBuffer s = new StringBuffer();
         s.append("Configuration Item: name = " + name + ", type = " + type);
+        s.append(", config value = " + getConfigValue());
+        s.append(", current value = " + getCurrentValue());
         if (allowedValues != null) {
             s.append(", allowedValues = " + allowedValues);
         }
@@ -57,6 +113,7 @@ public class ConfigItem {
         if (helpText != null) {
             s.append(", helpText = " + helpText);
         }
+        
         return s.toString();
     }
     
