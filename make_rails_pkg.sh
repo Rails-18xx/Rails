eb7868e9d88a51b11c39f56283617dcf9acec2aa
Stefan Frey <stefan.frey@web.de>
2011-09-23 08:09:31 +0200

    Prepared for release 1.5

---
+++ make_rails_pkg.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+if [ -z $1 ]; then
+	echo "Usage: $0 [version]"
+	exit
+fi
+
+home_dir="/home/freystef"
+pushd $home_dir/rails/git_repo/rails/
+
+tar cjpf $home_dir/rails-$1.tar.bz2 rails-$1
+zip -9 -r $home_dir/rails-$1.zip rails-$1
+
+popd
