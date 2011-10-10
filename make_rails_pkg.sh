22b79e84bee3711a29aaf25333be26f85a62a5bf
Stefan Frey <stefan.frey@web.de>
2011-10-10 20:40:19 +0200

    updated shell script to manage releases

--- make_rails_pkg.sh
+++ make_rails_pkg.sh
@@ -7,8 +7,8 @@ if [ -z $1 ]; then
 	exit
 fi
 
-home_dir="/home/freystef"
-pushd $home_dir/rails/git_repo/rails/
+home_dir="/home/freystef/rails/release"
+pushd $home_dir/git_repo/rails/
 
 tar cjpf $home_dir/rails-$1.tar.bz2 rails-$1
 zip -9 -r $home_dir/rails-$1.zip rails-$1
