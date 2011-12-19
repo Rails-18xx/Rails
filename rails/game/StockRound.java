db7c6ee7b80a57e88b8008ccec460fb91059ea87
Erik Vos <erik.vos@xs4all.nl>
2011-12-19 12:59:29 +0100

    Fixed initialisation of raiseIfSoldOut attribute.

--- rails/game/StockRound.java
+++ rails/game/StockRound.java
@@ -56,7 +56,7 @@ public class StockRound extends Round {
 
     /* Rules */
     protected int sequenceRule;
-    protected boolean raiseIfSoldOut;
+    protected boolean raiseIfSoldOut = false;
 
     /**
      * Constructor with the GameManager, will call super class (Round's) Constructor to initialize
@@ -72,13 +72,15 @@ public class StockRound extends Round {
 
         sequenceRule = getGameParameterAsInt(GameDef.Parm.STOCK_ROUND_SEQUENCE);
 
-        raiseIfSoldOut = true;
-
         guiHints.setVisibilityHint(GuiDef.Panel.MAP, true);
         guiHints.setVisibilityHint(GuiDef.Panel.STOCK_MARKET, true);
         guiHints.setActivePanel(GuiDef.Panel.STATUS);
     }
 
+    /** Start the Stock Round. <p>
+     * Please note: subclasses that are NOT real stock rounds should NOT call this method
+     * (or set raiseIfSoldOut to false after calling this method).
+     */
     public void start() {
 
         ReportBuffer.add(LocalText.getText("StartStockRound",
@@ -90,6 +92,8 @@ public class StockRound extends Round {
                 startingPlayer.getName() ));
 
         initPlayer();
+
+        raiseIfSoldOut = true;
 
     }
 
