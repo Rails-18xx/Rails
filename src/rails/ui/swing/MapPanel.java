fa259727ae46d8de921011ced8cc410b63462b9d
Frederick Weld <frederick.weld@gmail.com>
2012-01-10 12:16:54 +0100

    Added round-off to the fit-to-xyz zoom (continuous zoom, scroll bar)
    The current logic of discrete zoom steps is still valid - but it is
    complemented by a continuous adjustment factor ("last mile adjustment").
    
    In addition, the appearance and width of the scroll bar is now
    considered.
    (cherry picked from commit 38e4a6e05fae1349af459ed47c438271dc7373da)

--- src/rails/ui/swing/MapPanel.java
+++ src/rails/ui/swing/MapPanel.java
@@ -21,11 +21,14 @@ import rails.ui.swing.hexmap.*;
 public class MapPanel extends JPanel {
 
     private static final long serialVersionUID = 1L;
+    //defines how many pixels should be left as safety margin when calculating fit zooms
+    private static final int zoomFitSafetyMargin = 4; 
+            
     private MapManager mmgr;
     private HexMap map;
     private HexMapImage mapImage;
     private JScrollPane scrollPane;
-   
+    
     private GameUIManager gameUIManager;
     
     private JLayeredPane layeredPane;
@@ -133,8 +136,80 @@ public class MapPanel extends JPanel {
         adjustToNewMapZoom();
     }
     
-    private void zoomFit (boolean fitWidth, boolean fitHeight) {
-        map.zoomFit (getSize(), fitWidth, fitHeight);
+    /**
+     * Zoom-to-fit functionality is based on the discrete zoom steps.
+     * In order to achieve correctly fitting zoom, continuous adjustment factors are
+     * determined on top of that. 
+     */
+    private void zoomFit (boolean fitToWidth, boolean fitToHeight) {
+        ImageLoader imageLoader = GameUIManager.getImageLoader();
+        int zoomStep = map.getZoomStep();
+
+        //reset adjustment factor
+        imageLoader.resetAdjustmentFactor();
+
+        //determine the available size to fit to
+        //(double needed for subsequent calculations)
+        double width = getSize().width - zoomFitSafetyMargin;
+        double height = getSize().height - zoomFitSafetyMargin;
+
+        double idealFactorWidth = width / originalMapSize.width;
+        double idealFactorHeight = height / originalMapSize.height;
+
+        //determine which dimension will be the critical one for the resize
+        boolean isWidthCritical = ( !fitToHeight
+                || (fitToWidth && idealFactorWidth < idealFactorHeight));
+        
+        //check whether scrollbar will appear in the fit-to dimension and
+        //reduce available size accordingly (not relevant for fit-to-window)
+        if (isWidthCritical && idealFactorWidth > idealFactorHeight) {
+            width -= scrollPane.getVerticalScrollBar().getPreferredSize().width;
+            idealFactorWidth = width / originalMapSize.width;
+        }
+        if (!isWidthCritical && idealFactorWidth < idealFactorHeight) {
+            height -= scrollPane.getHorizontalScrollBar().getPreferredSize().height;
+            idealFactorHeight = height / originalMapSize.height;
+        }
+        
+        //abort resize if no space available
+        if (width < 0 || height < 0) return;
+
+        //increase zoomFactor until constraints do not hold
+        //OR zoom cannot be increased any more
+        while
+            (
+                    (
+                            (!fitToWidth || idealFactorWidth > imageLoader.getZoomFactor(zoomStep))
+                            &&
+                            (!fitToHeight || idealFactorHeight > imageLoader.getZoomFactor(zoomStep))
+                    )
+                    &&
+                    imageLoader.getZoomFactor(zoomStep+1) != imageLoader.getZoomFactor(zoomStep)
+            )
+            zoomStep++;
+        
+        //decrease zoomFactor until constraints do hold
+        //OR zoom cannot be decreased any more
+        while
+            (
+                    (
+                            (fitToWidth && idealFactorWidth < imageLoader.getZoomFactor(zoomStep))
+                            ||
+                            (fitToHeight && idealFactorHeight < imageLoader.getZoomFactor(zoomStep))
+                    )
+                    &&
+                    imageLoader.getZoomFactor(zoomStep-1) != imageLoader.getZoomFactor(zoomStep)
+            )
+            zoomStep--;
+        
+        //Determine and apply adjustment factor for precise fit
+        double idealFactor = isWidthCritical ? idealFactorWidth : idealFactorHeight;
+        imageLoader.setZoomAdjustmentFactor (
+            idealFactor / imageLoader.getZoomFactor(zoomStep));
+        
+        //trigger zoom execution
+        map.setZoomStep(zoomStep);
+
         adjustToNewMapZoom();
     }
     
