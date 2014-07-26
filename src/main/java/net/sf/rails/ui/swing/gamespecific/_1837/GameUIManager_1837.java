4247ec1441115750161247678a942879a6556ffe
Martin Brumm <dr.martin.brumm@t-online.de>
2014-07-26 12:19:13 +0200

    Created more StartroundClasses needed Currently by GameUI to start new StartRounds.
    StartRound and first Stockround are now playable.
    Conflicts:
    	src/main/resources/data/1837/CompanyManager.xml

---
+++ src/main/java/net/sf/rails/ui/swing/gamespecific/_1837/GameUIManager_1837.java
@@ -0,0 +1,20 @@
+package net.sf.rails.ui.swing.gamespecific._1837;
+
+import net.sf.rails.ui.swing.GameUIManager;
+
+public class GameUIManager_1837 extends GameUIManager {
+
+    public GameUIManager_1837() {
+        // TODO Auto-generated constructor stub
+    }
+    /* (non-Javadoc)
+     * @see net.sf.rails.ui.swing.GameUIManager#updateUI()
+     */
+    @Override
+    public void updateUI() {
+        // TODO Auto-generated method stub
+        super.updateUI();
+    }
+
+
+}
