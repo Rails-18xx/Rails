b3fd6726e1b45a11d605192a9458a545eeae10aa
Martin Brumm <dr.martin.brumm@t-online.de>
2014-07-26 22:41:29 +0200

    Preparing OperatingRound Class (without functon yet).

---
+++ src/main/java/net/sf/rails/game/specific/_1837/OperatingRound_1837.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package net.sf.rails.game.specific._1837;
+
+import net.sf.rails.game.GameManager;
+import net.sf.rails.game.OperatingRound;
+
+/**
+ * @author Martin
+ *
+ */
+public class OperatingRound_1837 extends OperatingRound {
+
+    /**
+     * @param parent
+     * @param id
+     */
+    public OperatingRound_1837(GameManager parent, String id) {
+        super(parent, id);
+        // TODO Auto-generated constructor stub
+    }
+
+    /* (non-Javadoc)
+     * @see net.sf.rails.game.OperatingRound#payout(int)
+     */
+    @Override
+    public void payout(int amount) {
+        // TODO Auto-generated method stub
+        super.payout(amount);
+    }
+
+}
