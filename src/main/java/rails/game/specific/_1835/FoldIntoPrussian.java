29b01bc0022155f57057fd273d4d8322c236e6a3
Martin Brumm <Dr.Martin.Brumm@t-online.de>
2014-07-27 10:59:14 +0200

    Creating a generic Folding Class for Nationals Adapting 1835 code to use that class.
    This is in preparation for 1837s three nationals.

--- src/main/java/rails/game/specific/_1835/FoldIntoPrussian.java
+++ src/main/java/rails/game/specific/_1835/FoldIntoPrussian.java
@@ -1,133 +1,27 @@
 package rails.game.specific._1835;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import rails.game.action.PossibleAction;
+import rails.game.action.FoldIntoNational;
 import net.sf.rails.game.Company;
-import net.sf.rails.game.CompanyManager;
-import net.sf.rails.util.RailsObjects;
-import net.sf.rails.util.Util;
-
-import com.google.common.base.Objects;
 
 /**
  * Rails 2.0: Updated equals and toString methods
 */
-public class FoldIntoPrussian extends PossibleAction {
+public class FoldIntoPrussian extends FoldIntoNational {
 
-    // Server settings
-    protected transient List<Company> foldableCompanies = null;
-    protected String foldableCompanyNames = null;
-
-    // Client settings
-    protected transient List<Company> foldedCompanies = null;
-    protected String foldedCompanyNames = null;
-
-    public static final long serialVersionUID = 1L;
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
 
     public FoldIntoPrussian(List<Company> companies) {
-        this.foldableCompanies = companies;
-        foldableCompanyNames = Util.joinNamesWithDelimiter(foldableCompanies, ",");
+       super(companies);
     }
 
     public FoldIntoPrussian(Company company) {
-        this (Arrays.asList(new Company[] {company}));
+        super(company);
     }
-
-    public List<Company> getFoldedCompanies() {
-        return foldedCompanies;
-    }
-
-
-    public void setFoldedCompanies(List<Company> foldedCompanies) {
-        this.foldedCompanies = foldedCompanies;
-        foldedCompanyNames = Util.joinNamesWithDelimiter (foldedCompanies, ",");
-    }
-
-
-    public List<Company> getFoldableCompanies() {
-        return foldableCompanies;
-    }
-
-
-    public String getFoldableCompanyNames() {
-        return foldableCompanyNames;
-    }
-
-
-    public String getFoldedCompanyNames() {
-        return foldedCompanyNames;
-    }
-
-    @Override
-    protected boolean equalsAs(PossibleAction pa, boolean asOption) {
-        // identity always true
-        if (pa == this) return true;
-        //  super checks both class identity and super class attributes
-        if (!super.equalsAs(pa, asOption)) return false; 
-
-        // check asOption attributes
-        FoldIntoPrussian action = (FoldIntoPrussian)pa; 
-        boolean options = 
-                Objects.equal(this.foldableCompanies, action.foldableCompanies) ||
-                    // additional conditions required as there is no sorting defined in old save files
-                    this.foldableCompanies != null && action.foldableCompanies != null
-                    && this.foldableCompanies.containsAll(action.foldableCompanies)
-                    && action.foldableCompanies.containsAll(this.foldableCompanies)
-        ;
-        
-        // finish if asOptions check
-        if (asOption) return options;
-        
-        // check asAction attributes
-        return options
-                && Objects.equal(this.foldedCompanies, action.foldedCompanies) ||
-                // additional conditions required as there is no sorting defined in old save files
-                this.foldedCompanies != null && action.foldedCompanies != null
-                && this.foldedCompanies.containsAll(action.foldedCompanies)
-                && action.foldedCompanies.containsAll(this.foldedCompanies)
-        ;
-    }
-
-    @Override
-    public String toString() {
-        return super.toString() + 
-                RailsObjects.stringHelper(this)
-                    .addToString("foldableCompanies", foldableCompanies)
-                    .addToStringOnlyActed("foldedCompanies", foldedCompanies)
-                    .toString()
-        ;
-    }
-    
-    /** Deserialize */
-    private void readObject(ObjectInputStream in) throws IOException,
-            ClassNotFoundException {
-        
-        Company company;
-
-        in.defaultReadObject();
-
-        CompanyManager cmgr = getCompanyManager();
-        if (foldableCompanyNames != null) {
-            foldableCompanies = new ArrayList<Company>();
-            for (String name : foldableCompanyNames.split(",")) {
-                company = cmgr.getPublicCompany(name);
-                if (company == null) company = cmgr.getPrivateCompany(name);
-                if (company != null) foldableCompanies.add(company);
-            }
-        }
-        if (Util.hasValue(foldedCompanyNames)) {
-            foldedCompanies = new ArrayList<Company>();
-            for (String name : foldedCompanyNames.split(",")) {
-                company = cmgr.getPublicCompany(name);
-                if (company == null) company = cmgr.getPrivateCompany(name);
-                if (company != null) foldedCompanies.add(company);
-            }
-        }
-    }
-
 }
+
+ 
