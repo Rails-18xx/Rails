6faeb4c0b9d62b9dada07045d29ae86e253de399
Martin Brumm <dr.martin.brumm@t-online.de>
2014-08-18 12:37:11 +0200

    Cleaning unused imports Conflicts: 	src/main/java/net/sf/rails/game/specific/_1837/OperatingRound_1837.java

--- src/main/java/net/sf/rails/game/specific/_1837/OperatingRound_1837.java
+++ src/main/java/net/sf/rails/game/specific/_1837/OperatingRound_1837.java
@@ -3,15 +3,13 @@
  */
 package net.sf.rails.game.specific._1837;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.SortedMap;
-import java.util.TreeMap;
+
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Table;
+
 
 import net.sf.rails.common.LocalText;
 import net.sf.rails.common.ReportBuffer;
@@ -21,13 +19,13 @@ import net.sf.rails.game.OperatingRound;
 import net.sf.rails.game.Phase;
 import net.sf.rails.game.Player;
 import net.sf.rails.game.PublicCompany;
-import net.sf.rails.game.Train;
-import net.sf.rails.game.TrainType;
 import net.sf.rails.game.special.ExchangeForShare;
 import net.sf.rails.game.special.SpecialProperty;
-import net.sf.rails.game.special.SpecialTrainBuy;
 import net.sf.rails.game.state.BooleanState;
 import net.sf.rails.game.state.MoneyOwner;
+
+
+
 
 /**
  * @author Martin
