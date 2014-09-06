4f8ef32fdddc1b86e8551a4917a518055af8f443
Michael Alexander <outsidepasser@gmail.com>
2014-09-06 12:46:16 -0400

    Added ParliamentCharter

---
+++ src/main/java/net/sf/rails/game/specific/_1862/ParliamentCharter.java
@@ -0,0 +1,13 @@
+package net.sf.rails.game.specific._1862;
+
+import net.sf.rails.game.PrivateCompany;
+import net.sf.rails.game.RailsItem;
+
+// FIXME: Move static field numberOfPrivateCompanies to CompanyManager
+
+public class ParliamentCharter extends PrivateCompany {
+
+    public ParliamentCharter(RailsItem parent, String id) {
+        super(parent, id);
+    }
+}
