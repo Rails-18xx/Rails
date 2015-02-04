0e925f91b92ed6484d5cbfe6b32f7a5e821c85b2
Stefan Frey <stefan.frey@web.de>
2015-02-04 16:04:21 +0100

    rewrote ExpressTrainModifier, activated old StockMarketBonus

--- rails/game/specific/_1880/ExpressTrainModifier.java
+++ rails/game/specific/_1880/ExpressTrainModifier.java
@@ -1,6 +1,6 @@
 package rails.game.specific._1880;
 
-
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -9,6 +9,7 @@ import rails.algorithms.NetworkVertex;
 import rails.algorithms.RevenueAdapter;
 import rails.algorithms.RevenueDynamicModifier;
 import rails.algorithms.RevenueTrainRun;
+import rails.common.LocalText;
 
     /**
      * @author Martin Brumm - 2015-01-08
@@ -17,13 +18,6 @@ import rails.algorithms.RevenueTrainRun;
     public class ExpressTrainModifier implements RevenueDynamicModifier {
 
         private boolean hasExpress;
-
-        /**
-         * 
-         */
-        public ExpressTrainModifier() {
-            // TODO Auto-generated constructor stub
-        }
 
         public boolean prepareModifier(RevenueAdapter revenueAdapter) {
             // 1. check if there is a Express Train in the train set
@@ -46,58 +40,83 @@ import rails.algorithms.RevenueTrainRun;
             return 0;
         }
 
+        // TODO: Rails 2.0 rewrite this by using Guava
+        private List<NetworkVertex> extractExpressRun(RevenueTrainRun run, int length) {
+            
+            // check for valid run first
+            if (!run.hasAValidRun()) return new ArrayList<NetworkVertex>();
+            
+            NetworkVertex baseVertex = run.getFirstVertex();
+   
+            // create a copy of runlist, Collections.sorts has side effects
+            List<NetworkVertex> nextVertices = new ArrayList<NetworkVertex>(
+                    run.getRunVertices().subList(1, run.getRunVertices().size())); 
+            
+            
+            Collections.sort(nextVertices); // Sort the Vertices by Value
+            List<NetworkVertex> expressVertices = nextVertices.subList(0, Math.min(nextVertices.size(),length-1));
+            expressVertices.add(0, baseVertex);
+            
+            return expressVertices;
+        }
+        
+        
         public int evaluationValue(List<RevenueTrainRun> runs, boolean optimalRuns) {
+            int value = 0;
+            //Find out which Express Train is involved
+            for (RevenueTrainRun run:runs) {
 
-            int evaluationValue = 0;
-            if (hasExpress) {
-                int value = 0; 
-                //Find out which Express Train is involved
-                 for (RevenueTrainRun run:runs) {
-                     
-                      if ((run.getTrain().getRailsTrainType() != null) 
+                if ((run.getTrain().getRailsTrainType() != null) 
                         && (run.getTrain().getRailsTrainType().getCertificateType().getName().equals("6E"))) {
-                        /*  We Found a run with an express Train, now we have to make sure that the result gets trimmed
+                    /*  We Found a run with an express Train, now we have to make sure that the result gets trimmed
                          to the maximum allowed stations, cause so far its a Diesel Train !
-                        */
-                         List<NetworkVertex> preExpressVertices = run.getRunVertices();
-                        Collections.sort(preExpressVertices); // Sort the Vertices by Value
-                        // Now we need to throw out all Values beyond the first 6 and reset the run vertices...
-                        int i=0;
-                        for (NetworkVertex preExpressVertex:preExpressVertices) {
-                            i=i+1;
-                            if (i <=6) {
-                                value += preExpressVertex.getValue(); //Value of the 6E Run
-                            }
-                        }
-                     }
-                     if ((run.getTrain().getRailsTrainType() != null) 
-                             && (run.getTrain().getRailsTrainType().getCertificateType().getName().equals("8E"))) {
-                              // We Found a run with an express Train, now we have to make sure that the result gets trimmed
-                              // to the maximum allowed stations, cause so far its a Diesel Train !
-                             List<NetworkVertex> preExpressVertices =  run.getRunVertices();
-                             Collections.sort(preExpressVertices); // Sort the Vertices by Value
-                             // Now we need to throw out all Values beyond the first 6 and reset the run vertices...
-                             int i=0;
-                             for (NetworkVertex preExpressVertex:preExpressVertices) {
-                                 i=i+1;
-                                 if (i <=8) {
-                                     value = value + preExpressVertex.getValue(); //Value of the 8E Run combined with 6E
-                                 }
-                             }
-                             
-                          }
-                     if (value !=0) {
-                         evaluationValue -= value;
-                     }
-                 }
-                 return evaluationValue;
+                     */
+                    int expressRunValue = NetworkVertex.sum(extractExpressRun(run, 6));
+                    value += expressRunValue - run.getRunValue();
+
+                }
+                if ((run.getTrain().getRailsTrainType() != null) 
+                        && (run.getTrain().getRailsTrainType().getCertificateType().getName().equals("8E"))) {
+                    // We Found a run with an express Train, now we have to make sure that the result gets trimmed
+                    // to the maximum allowed stations, cause so far its a Diesel Train !
+                    int expressRunValue = NetworkVertex.sum(extractExpressRun(run, 8));
+                    value += expressRunValue - run.getRunValue();
+                }
             }
-                        
-            return 0;
+            return value;
         }
 
-        public void adjustOptimalRun(List<RevenueTrainRun> optimalRuns) {
+        // this does not work yet, requires adjustments in underlying revenue code
+        // TODO: Rails 2.0
+//        private void adjustExpressRun(RevenueTrainRun run, List<NetworkVertex> expressVertices) {
+//            for (NetworkVertex vertex:new ArrayList<NetworkVertex>(run.getRunVertices())) {
+//                if (!expressVertices.contains(vertex)) {
+//                   run.getRunVertices().remove(vertex);
+//                }
+//            }
+//        }
 
+        public void adjustOptimalRun(List<RevenueTrainRun> optimalRuns) {
+            // this does not work yet, requires adjustments in underlying revenue code
+            // TODO: Rails 2.0
+            
+//            //Find out which Express Train is involved
+//            for (RevenueTrainRun run:optimalRuns) {
+//                
+//                if ((run.getTrain().getRailsTrainType() != null) 
+//                        && (run.getTrain().getRailsTrainType().getCertificateType().getName().equals("6E"))) {
+//                    /*  We Found a run with an express Train, now we have to make sure that the result gets trimmed
+//                         to the maximum allowed stations, cause so far its a Diesel Train !
+//                     */
+//                    adjustExpressRun(run, extractExpressRun(run, 6));
+//                }
+//                if ((run.getTrain().getRailsTrainType() != null) 
+//                        && (run.getTrain().getRailsTrainType().getCertificateType().getName().equals("8E"))) {
+//                    // We Found a run with an express Train, now we have to make sure that the result gets trimmed
+//                    // to the maximum allowed stations, cause so far its a Diesel Train !
+//                    adjustExpressRun(run, extractExpressRun(run, 8));
+//                }
+//            }
             
         }
 
@@ -110,7 +129,7 @@ import rails.algorithms.RevenueTrainRun;
         }
 
         public String prettyPrint(RevenueAdapter revenueAdapter) {
-            return null;
+            return LocalText.getText("1880ExpressAdjustment", evaluationValue(revenueAdapter.getOptimalRun(), true));
         }
 
  
--- rails/game/specific/_1880/StockMarketBonusModifier.java
+++ rails/game/specific/_1880/StockMarketBonusModifier.java
@@ -5,31 +5,32 @@ import java.util.List;
 import rails.algorithms.RevenueAdapter;
 import rails.algorithms.RevenueDynamicModifier;
 import rails.algorithms.RevenueTrainRun;
+import rails.common.LocalText;
 import rails.game.PublicCompanyI;
+
+// TODO: Rails 2.0 allow for a simpler modifier to change the revenue value
 
 public class StockMarketBonusModifier implements RevenueDynamicModifier {
 
     private PublicCompanyI company;
+    private int value;
     
+    // activate only for 1880 public companies
     public boolean prepareModifier(RevenueAdapter revenueAdapter) {
         company = revenueAdapter.getCompany();
-        return true;
+        if (company instanceof PublicCompany_1880) {
+            value = company.getCurrentSpace().getType().hasAddRevenue()*10;
+            return true;
+        }
+        return false;
     }
     
     public int predictionValue() {
-        if (company instanceof PublicCompany_1880) {
-            return company.getCurrentSpace().getType().hasAddRevenue()*10;
-        } else {
-            return 0;
-        }
+        return value;
     }
 
     public int evaluationValue(List<RevenueTrainRun> runs, boolean optimalRuns) {
-        if (company instanceof PublicCompany_1880) {
-            return company.getCurrentSpace().getType().hasAddRevenue()*10;
-        } else {
-            return 0;
-        }
+        return value;
     }
 
     public void adjustOptimalRun(List<RevenueTrainRun> optimalRuns) {        
@@ -44,7 +45,7 @@ public class StockMarketBonusModifier implements RevenueDynamicModifier {
     }
 
     public String prettyPrint(RevenueAdapter revenueAdapter) {
-        return null;
+        return LocalText.getText("1880StockMarketBonus", value);
     }
 
 }
