8801ecbb59f9cc7ae6b97c60634eb4803f7a32f9
Stefan Frey <stefan.frey@web.de>
2015-07-08 21:59:08 +0200

    fixed bug that on undo of a tile lay the route graph was not updated

--- src/main/java/net/sf/rails/algorithms/NetworkAdapter.java
+++ src/main/java/net/sf/rails/algorithms/NetworkAdapter.java
@@ -11,6 +11,9 @@ import org.slf4j.LoggerFactory;
 
 /**
  * This class coordinates the creation of company related graphs
+ * 
+ * TODO: NetworkAdapter should be changed to a NetworkManager and include functionality currently in NetworkGraph
+ * e.g. NetworkGraphModifier
  */
 public class NetworkAdapter {
 
@@ -18,14 +21,6 @@ public class NetworkAdapter {
             LoggerFactory.getLogger(NetworkAdapter.class);
 
     private final RailsRoot root;
-    
-    private NetworkGraph mapGraph;
-    private NetworkGraph routeGraph;
-    private NetworkGraph revenueGraph;
-    private NetworkMultigraph multiGraph;
-
-    private PublicCompany company;
-    private boolean addHQ;
     
     private NetworkAdapter(RailsRoot root) {
         this.root = root;
@@ -36,42 +31,20 @@ public class NetworkAdapter {
     }
     
     public NetworkGraph getMapGraph() {
-        if (mapGraph == null) {
-            mapGraph = NetworkGraph.createMapGraph(root);
-            log.info("MapGraph created");
-        }
-        return mapGraph;
+        return NetworkGraph.createMapGraph(root);
     }
         
     public NetworkGraph getRouteGraph(PublicCompany company, boolean addHQ) {
-        if (routeGraph == null || company != this.company || addHQ != this.addHQ) {
-            routeGraph = NetworkGraph.createRouteGraph(getMapGraph(), company, addHQ);
-            this.company = company;
-            this.addHQ = addHQ;
-            log.info("RouteGraph created");
-        }
-        return routeGraph;
+        return NetworkGraph.createRouteGraph(getMapGraph(), company, addHQ);
     }
     
-    public NetworkGraph getRevenueGraph(PublicCompany company,
-            Collection<NetworkVertex> protectedVertices) {
-        if (revenueGraph == null) {
-            revenueGraph = NetworkGraph.createOptimizedGraph(getRouteGraph(company, false),
-                    protectedVertices);
-            log.info("RevenueGraph created");
-        }
-        
-        return revenueGraph;
+    public NetworkGraph getRevenueGraph(PublicCompany company, Collection<NetworkVertex> protectedVertices) {
+        return NetworkGraph.createOptimizedGraph(getRouteGraph(company, false), protectedVertices);
     }
     
     public NetworkMultigraph getMultigraph(PublicCompany company,
             Collection<NetworkVertex> protectedVertices) {
-        if (multiGraph == null) {
-            multiGraph = NetworkMultigraph.create(
-                    getRevenueGraph(company, protectedVertices), protectedVertices);
-            log.info("MultiGraph created");
-        }
-        return multiGraph;
+        return NetworkMultigraph.create(getRevenueGraph(company, protectedVertices), protectedVertices);
     }
     
 }
