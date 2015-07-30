a78f0ba876c4553d5be1a406db751b3fe4a44946
Stefan Frey <stefan.frey@web.de>
2015-07-30 14:01:37 +0200

    fixed bug that Prussian stock price does not increase as long as there are un-exchanged shares

--- src/main/java/net/sf/rails/game/specific/_1835/PublicCompany_1835.java
+++ src/main/java/net/sf/rails/game/specific/_1835/PublicCompany_1835.java
@@ -1,45 +1,31 @@
 package net.sf.rails.game.specific._1835;
 
-import net.sf.rails.common.GameOption;
+import net.sf.rails.game.Bank;
 import net.sf.rails.game.BankPortfolio;
 import net.sf.rails.game.PublicCertificate;
 import net.sf.rails.game.PublicCompany;
 import net.sf.rails.game.RailsItem;
 import net.sf.rails.game.state.Owner;
-/**
- * 
- */
+
 public class PublicCompany_1835 extends PublicCompany {
 
 
-    /**
-     * @author Martin
-     *
-     */
     public PublicCompany_1835(RailsItem parent, String Id) {
         super(parent, Id);
-        // TODO Auto-generated constructor stub
     }
 
-    /* (non-Javadoc)
-     * @see rails.game.PublicCompany#isSoldOut()
-     */
     @Override
     public boolean isSoldOut() {
-        Owner owner;
-        String name;
+        // sold out makes only sense for buyable companies  
+        if (!isBuyable()) return false;
 
-        if ("yes".equalsIgnoreCase(GameOption.getValue(this, "PrussianReservedIgnored"))) {
-            for (PublicCertificate cert : certificates.view()) {
-                owner = cert.getOwner();
-                name = cert.getName();
-                if ((owner instanceof BankPortfolio || owner == cert.getCompany()) && (!name.equalsIgnoreCase("unavailable"))) {
-                    return false;
-                }
+        for (PublicCertificate cert : certificates.view()) {
+            Owner owner = cert.getOwner();
+            // check if any shares are in the bank (except unavailable for reserved shares)
+            if (owner instanceof BankPortfolio && owner != Bank.getUnavailable(this)) {
+                return false;
             }
-            return true;
         }
-        return super.isSoldOut();
+        return true;
     }
-
 }
