63b19d8ca2f271ce5aacd7051a2fe3254d2492aa
Dr. Martin Brumm <dr.martin.brumm@t-online.de>
2015-08-23 18:00:02 +0200

    Setup Classes needed for 1844 (imho) Comments and Feedback is welcome

---
+++ src/main/java/net/sf/rails/game/specific/_1844/TunnelCompany.java
@@ -0,0 +1,13 @@
+package net.sf.rails.game.specific._1844;
+
+import net.sf.rails.game.PrivateCompany;
+import net.sf.rails.game.RailsItem;
+
+public class TunnelCompany extends PrivateCompany {
+
+    public TunnelCompany(RailsItem parent, String id) {
+        super(parent, id);
+        // TODO Auto-generated constructor stub
+    }
+
+}
