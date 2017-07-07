3d8d6843e8e5648822d68274298b011d9e2acf9c
Dr. Martin Brumm <dr.martin.brumm@t-online.de>
2017-07-07 20:08:38 +0200

    Working on Fixes for the BonusToken Situation in 18AL

--- src/main/java/net/sf/rails/ui/swing/gamespecific/_18AL/ORUIManager_18AL.java
+++ src/main/java/net/sf/rails/ui/swing/gamespecific/_18AL/ORUIManager_18AL.java
@@ -2,29 +2,26 @@ package net.sf.rails.ui.swing.gamespecific._18AL;
 
 import java.util.List;
 
-import rails.game.action.PossibleAction;
+import com.google.common.collect.Multimap;
 
+import rails.game.action.LayBonusToken;
+import rails.game.action.LayToken;
+import rails.game.action.PossibleAction;
+import net.sf.rails.algorithms.NetworkGraph;
+import net.sf.rails.game.MapHex;
+import net.sf.rails.game.PublicCompany;
+import net.sf.rails.game.Stop;
 import net.sf.rails.game.specific._18AL.AssignNamedTrains;
 import net.sf.rails.ui.swing.ORUIManager;
+import net.sf.rails.ui.swing.hexmap.GUIHex;
+import net.sf.rails.ui.swing.hexmap.HexMap;
+import net.sf.rails.ui.swing.hexmap.TokenHexUpgrade;
 
 
 public class ORUIManager_18AL extends ORUIManager {
 
-    /*
-    @Override
-    protected void checkForGameSpecificActions() {
-
-        if (possibleActions.contains(AssignNamedTrains.class)) {
-
-            log.debug("AssignNamedTrain possible action found");
-            for (AssignNamedTrains action : possibleActions.getType(AssignNamedTrains.class)) {
-                String text = action.toMenu();
-                orPanel.addSpecialAction(action, text);
-            }
-        }
-    }
-    */
-
+    
+    
     protected boolean processGameSpecificActions(List<PossibleAction> actions) {
 
         Class<? extends PossibleAction> actionType = actions.get(0).getClass();
@@ -43,6 +40,35 @@ public class ORUIManager_18AL extends ORUIManager {
         }
 
         return false;
+    }
+
+    /* (non-Javadoc)
+     * @see net.sf.rails.ui.swing.ORUIManager#addLocatedTokenLays(rails.game.action.LayToken)
+     */
+   @Override
+    protected void addLocatedTokenLays(LayToken action) {
+  //TODO: Rework for general setup as special property 
+        
+        
+        if (action instanceof LayBonusToken) { //Special Action from Private Company in 18AL
+            PublicCompany company = action.getCompany();
+            NetworkGraph graph = networkAdapter.getRouteGraph(company, true);
+            
+            for (MapHex hex:action.getLocations()) {
+                if (graph.getPassableStations().containsKey(hex) )
+                {
+                    GUIHex guiHex = orWindow.getMapPanel().getMap().getHex(hex);
+                    TokenHexUpgrade upgrade = TokenHexUpgrade.create(
+                            guiHex, hex.getTokenableStops(action.getCompany()), action);
+                    TokenHexUpgrade.validates(upgrade);
+                    hexUpgrades.put(guiHex, upgrade);
+                    continue;
+                }
+            }
+        }
+        else {
+        super.addLocatedTokenLays(action);
+        }
     }
 
 }
