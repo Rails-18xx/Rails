b69ba432665ff5e0df74f41d92a1edf030e04986
Martin Brumm <dr.martin.brumm@t-online.de>
2020-06-26 16:43:54 +0200

    2.2.2-First step to make 1880 playable again

--- src/main/java/net/sf/rails/ui/swing/gamespecific/_1880/StatusWindow_1880.java
+++ src/main/java/net/sf/rails/ui/swing/gamespecific/_1880/StatusWindow_1880.java
@@ -1,5 +1,6 @@
 package net.sf.rails.ui.swing.gamespecific._1880;
 
+import net.sf.rails.game.StartRound;
 import net.sf.rails.game.specific._1880.GameManager_1880;
 import net.sf.rails.game.specific._1880.ParSlotManager;
 import net.sf.rails.ui.swing.GameUIManager;
@@ -33,10 +34,11 @@ public class StatusWindow_1880 extends StatusWindow {
 
         pane_1880.add(parSlotsPanel.getPanel());
 
-        investorPanel = new InvestorPanel((GameManager_1880) gameUIManager.getGameManager());
+        if (!(gameUIManager.getGameManager().getCurrentRound() instanceof StartRound)) {
+            investorPanel = new InvestorPanel((GameManager_1880) gameUIManager.getGameManager());
 
-        pane_1880.add(investorPanel.getPanel());
-
+            pane_1880.add(investorPanel.getPanel());
+        }
         pane.add(pane_1880, BorderLayout.EAST);
 
     }
