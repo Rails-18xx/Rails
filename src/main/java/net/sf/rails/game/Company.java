706ca32b92acc8cccf47c7cb680a79adbc3a1550
Erik Vos <erik.vos@xs4all.nl>
2021-04-07 14:57:34 +0200

    1837: Added mine revenue to coal company info

--- src/main/java/net/sf/rails/game/Company.java
+++ src/main/java/net/sf/rails/game/Company.java
@@ -12,18 +12,18 @@ import com.google.common.collect.ImmutableSet;
 public interface Company extends RailsOwner, Configurable, Cloneable {
 
     /** The name of the XML tag used to configure a company. */
-    public static final String COMPANY_ELEMENT_ID = "Company";
+    String COMPANY_ELEMENT_ID = "Company";
 
     /** The name of the XML attribute for the company's name. */
-    public static final String COMPANY_NAME_TAG = "name";
+    String COMPANY_NAME_TAG = "name";
 
     /** The name of the XML attribute for the company's type. */
-    public static final String COMPANY_TYPE_TAG = "type";
+    String COMPANY_TYPE_TAG = "type";
 
     /**
      * A Comparator for Companies
      */
-    public static final Comparator<Company> COMPANY_COMPARATOR = new Comparator<Company>() {
+    Comparator<Company> COMPANY_COMPARATOR = new Comparator<Company>() {
         @Override
         public int compare(Company c0, Company c1) {
             return ComparisonChain.start()
@@ -33,79 +33,33 @@ public interface Company extends RailsOwner, Configurable, Cloneable {
         }
     };
 
-//    protected String longName;
-//    protected String alias = null; // To allow reloading files with old names after name changes
-//    protected CompanyType type;
-//    protected int companyNumber; // For internal use
-//
-//    protected String infoText = "";
-//    protected String parentInfoText = "";
-//
-//    /**
-//     * The value per certificate at the end of the rails.game. Default 0 (for
-//     * privates).
-//     */
-//    protected int value = 0;
-//    /**
-//     * Twice the amount each certificate counts against the limit (this way we
-//     * take care for certs that count for 0.5)
-//     */
-//    protected int certLimitCount = 2;
-//
-//    /** Closed state */
-//    protected final BooleanState closedObject = new BooleanState(this, "closed", false);
-//
-    /** Only to be called from subclasses */
-//    public void configureFromXML(Tag tag) throws ConfigurationException {
-//
-//        // Special properties
-//        Tag spsTag = tag.getChild("SpecialProperties");
-//        if (spsTag != null) {
-//
-//            List<Tag> spTags = spsTag.getChildren("SpecialProperty");
-//            String className;
-//            for (Tag spTag : spTags) {
-//                className = spTag.getAttributeAsString("class");
-//                if (!Util.hasValue(className))
-//                    throw new ConfigurationException(
-//                    "Missing class in private special property");
-//                String uniqueId = SpecialProperty.createUniqueId();
-//                SpecialProperty sp = Configure.create(SpecialProperty.class, className, this, uniqueId);
-//                sp.setOriginalCompany(this);
-//                sp.configureFromXML(spTag);
-//                sp.moveTo(this);
-//                parentInfoText += "<br>" + sp.getInfo();
-//            }
-//        }
-//    }
-
-    public void initType(CompanyType type);
+    void initType(CompanyType type);
 
     /**
      * @return Type of company (Public/Private)
      */
-    public CompanyType getType();
+    CompanyType getType();
 
     /**
      * @return whether this company is closed
      */
-    public boolean isClosed();
+    boolean isClosed();
 
     /**
      * Close this company.
      */
-    public void setClosed();
+    void setClosed();
 
-    public String getLongName();
+    default String getLongName() {return "";}
 
-    public String getAlias();
+    default String getAlias() {return "";}
 
-    public String getInfoText();
+    default String getInfoText() {return "";}
 
     // Since 1835 required for both private and public companies
     /**
      * @return Set of all special properties we have.
      */
-    public abstract ImmutableSet<SpecialProperty> getSpecialProperties();
+    ImmutableSet<SpecialProperty> getSpecialProperties();
 
 }
